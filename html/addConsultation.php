f4eeddf4b63a5d0049b27256fcec623344963b92
ratish <ratishisuri44@gmail.com>
2020-12-01 22:00:18 +0400

    unification

--- html/addConsultation.php
+++ html/addConsultation.php
@@ -1,7 +1,8 @@
 
 <?php include("header.php")?>
-
-    <h1 class="text-center display-4">Add Consultation Results</h1>
+     
+<h1 class="text-center display-4">Add Consultation Results</h1>
+ <div class="container"> 
     <form id="addConsultation" action="setconsultation.php" method="post">
     <div class="form-row">
         <div class="form-group col-md-4">
@@ -16,7 +17,6 @@
         <label>Room no.</label>
         <input type="text" name="slot" readonly class="form-control">
         </div>
-        </div>
     </div>
     <div class="form-row">
             <div class="form-group col-md-4">
@@ -26,7 +26,7 @@
     </div>
     <button type="submit" class="btn btn-primary">Add Consultation</button>
     </form>
-
+</div>
 
     <src="https://code.jquery.com/jquery-3.3.1.slim.min.js" integrity="sha384-q8i/X+965DzO0rT7abK41JStQIAqVgRVzpbzo5smXKp4YfRvH+8abtTE1Pi6jizo" crossorigin="anonymous"></src=>
     <src="https://cdnjs.cloudflare.com/ajax/libs/popper.js/1.14.7/umd/popper.min.js" integrity="sha384-UO2eT0CpHqdSJQ6hJty5KVphtPhzWj9WO1clHTMGa3JDZwrnQq4sF86dIHNDz0W1" crossorigin="anonymous"></src=>
--- html/footer.php
+++ html/footer.php
@@ -4,6 +4,61 @@
 
 
 </body>
+<!-- Footer -->
+<footer class="page-footer font-small">
+
+    
+  
+        </div>
+        <!-- Grid row-->
+  
+      </div>
+    </div>
+  
+    <!-- Footer Links -->
+    <div class="container text-center text-md-left mt-5">
+  
+      <!-- Grid row -->
+      <div class="row mt-3">
+  
+        <!-- Grid column -->
+        <div class="col-md-3 col-lg-4 col-xl-3 mx-auto mb-4">
+  
+          <!-- Content -->
+          <h6 class="text-uppercase font-weight-bold">Safeture</h6>
+          <hr class="deep-purple accent-2 mb-4 mt-0 d-inline-block mx-auto" style="width: 60px;">
+          <p>Hospital appointment system.</p>
+  
+        </div>
+       
+        <!-- Grid column -->
+        <div class="col-md-4 col-lg-3 col-xl-3 mx-auto mb-md-0 mb-4">
+  
+          <!-- Links -->
+          <h6 class="text-uppercase font-weight-bold">Important Contacts</h6>
+          <hr class="deep-purple accent-2 mb-4 mt-0 d-inline-block mx-auto" style="width: 60px;">
+          <p>
+            <i class="fas fa-phone mr-3"></i>yyy 57777777</p>
+          <p>
+            <i class="fas fa-phone mr-3"></i>xxx 56666666</p>
+          
+        </div>
+        <!-- Grid column -->
+  
+      </div>
+      <!-- Grid row -->
+  
+    </div>
+    <!-- Footer Links -->
+  
+    <!-- Copyright -->
+    <div class="footer-copyright text-center py-3">© 2020 Copyright:
+      <a href="#"> uni.com</a>
+    </div>
+    <!-- Copyright -->
+  
+  </footer>
+  <!-- Footer -->
 <script src="https://code.jquery.com/jquery-3.5.1.slim.min.js" integrity="sha384-DfXdz2htPH0lsSSs5nCTpuj/zy4C+OGpamoFVy38MVBnE+IbbVYUew+OrCXaRkfj" crossorigin="anonymous"></script>
 <script src="https://cdn.jsdelivr.net/npm/bootstrap@4.5.3/dist/js/bootstrap.bundle.min.js" integrity="sha384-ho+j7jyWK8fNQe+A12Hb8AhRq26LrZ/JpcUGGOn+Y7RsweNrtN/tE3MoK7ZeZDyx" crossorigin="anonymous"></script>
 
