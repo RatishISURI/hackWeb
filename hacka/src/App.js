c51099780ec764c810a1670ddb55bd8ed58d7e77
Ratish <ratishisuri44@gmail.com>
2020-12-01 14:16:07 +0400

    combine

--- hacka/src/App.js
+++ hacka/src/App.js
@@ -1,13 +1,17 @@
 import logo from './logo.svg';
 import './App.css';
+import Button from '@material-ui/core/Button'
 
 function App() {
   return (
     <div className="App">
       <header className="App-header">
+        <Button variant="contained" color="primary">
+          Hello
+          </Button> 
         <img src={logo} className="App-logo" alt="logo" />
         <p>
-          Edit <code>src/App.js</code> and save to reload.
+          Edit <code>src/App.js</code> and save llto reload.
         </p>
         <a
           className="App-link"
