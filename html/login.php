83ef0ffa6b5f1cbf8beb5549bc5867650f4a20fc
Ratish <ratishisuri44@gmail.com>
2020-12-01 19:03:20 +0400

    combine

--- html/login.php
+++ html/login.php
@@ -18,11 +18,11 @@
         <div class="collapse navbar-collapse" id="navbarSupportedContent">
           <ul class="navbar-nav mr-auto">
             <li class="nav-item active">
-              <a class="nav-link" href="index.html">Home <span class="sr-only">(current)</span></a>
+              <a class="nav-link" href="schedule.html">Appointments <span class="sr-only">(current)</span></a>
             </li>
             <li class="nav-item">
               <a class="nav-link" href="#
-              ">Link</a>
+              ">Make appointment</a>
             </li>
             <li class="nav-item dropdown my-2 my-lg-0">
               <a class="nav-link dropdown-toggle" href="#" id="navbarDropdown" role="button" data-toggle="dropdown" aria-haspopup="true" aria-expanded="false">
